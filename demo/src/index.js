fda36d84afa63f39077bc2c5c89183f3056ad4e7
Sammy Israwi <sammy.israwi@gmail.com>
2017-05-30 12:30:17 -0400

    Created a demo page

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,51 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import PhotoStream from '../../src'
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-photostream Demo</h1>
-      <Example/>
-    </div>
+    let listOfSquareImages = [
+      "http://lorempixel.com/400/400",
+      "http://lorempixel.com/400/400",
+      "http://lorempixel.com/400/400",
+      "http://lorempixel.com/400/400",
+      "http://lorempixel.com/400/400",
+      "http://lorempixel.com/400/400"
+    ];
+
+    let listOfNotSquareImages = [
+      "http://lorempixel.com/400/450",
+      "http://lorempixel.com/600/200",
+      "http://lorempixel.com/500/350",
+      "http://lorempixel.com/600/300",
+      "http://lorempixel.com/400/600",
+      "http://lorempixel.com/400/400"
+    ];
+
+    let listOfImageObjects = [
+      {image:"http://lorempixel.com/400/450", altText:"first one"},
+      {image:"http://lorempixel.com/600/200", altText:"second one"},
+      {image:"http://lorempixel.com/500/350", altText:"third one"},
+      {image:"http://lorempixel.com/600/300", altText:"fourth one"},
+      {image:"http://lorempixel.com/400/600", altText:"fifth one"},
+      {image:"http://lorempixel.com/400/400", altText:"sixth one"}
+    ];
+
+    return (
+      <div>
+        <h1>react-photostream Demo</h1>
+        <hr />
+        <h2>List of Square Images</h2>
+        <PhotoStream imageList={listOfSquareImages} />
+        <hr />
+        <h2>List of Not Square Images</h2>
+        <PhotoStream imageList={listOfNotSquareImages} />
+        <hr />
+        <h2>List of image objects "{"{image, altText}"}"</h2>
+        <PhotoStream imageList={listOfImageObjects} />
+      </div>
+    );
   }
 }
 

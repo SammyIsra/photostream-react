90023d54766aefdadd4057925bdbb099695af72a
Sammy Israwi <sammy.israwi@gmail.com>
2017-06-10 04:51:44 -0400

    Format of the event handlers is now different. If they want to be able to use 'this', they need to accept a second argumen

--- src/Photo.js
+++ src/Photo.js
@@ -2,22 +2,44 @@ import React from 'react';
 
 import './Photo.css';
 
-//Functional component of a photo in the stream
-const Photo = (props) => {
+//Component of a photo in the stream
+class Photo extends React.Component {
 
-    //prop photo
-    const {picture, altText, eventHandlers} = props;
+    constructor(props){
+        super(props);
+        this.ReturnBoundEventHandlerOrUndefined = this.ReturnBoundEventHandlerOrUndefined.bind(this)
+    }
 
-    return (
-        <img
-            className="photostream_photo"
-            src={picture}
-            alt={altText}
-            onMouseEnter={eventHandlers? eventHandlers.onMouseEnter : undefined}
-            onMouseLeave={eventHandlers? eventHandlers.onMouseLeave : undefined}
-            onClick={eventHandlers? eventHandlers.onClick : undefined}
-        />
-    );
+    //Return a function that takes 'event' and calls the eventHandler with that event as an arg
+    ReturnBoundEventHandlerOrUndefined(eventHandler){
+        if(!eventHandler)
+            return undefined;
+        return (event)=>{
+            eventHandler(event, this);
+        }
+    }
+
+    render(){
+
+        //Destructure values from props
+        const {picture, altText, eventHandlers} = this.props;
+
+        //Get properly structured functions for the event handlers from the provided ones
+        const onMouseEnterHandler = this.ReturnBoundEventHandlerOrUndefined(eventHandlers.onMouseEnter);
+        const onMouseLeaveHandler = this.ReturnBoundEventHandlerOrUndefined(eventHandlers.onMouseLeave);
+        const onClickHandler = this.ReturnBoundEventHandlerOrUndefined(eventHandlers.onClick);
+
+        return (
+            <img
+                className="photostream_photo"
+                src={picture}
+                alt={altText}
+                onMouseEnter={onMouseEnterHandler}
+                onMouseLeave={onMouseLeaveHandler}
+                onClick={onClickHandler}
+            />
+        );
+    }
 }
 
 export default Photo;
